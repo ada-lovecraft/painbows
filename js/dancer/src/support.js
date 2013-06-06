6f3a478f39a9df835aee959923d0c41c89262488
Jeremy Dowell <jdowell@fossil.com>
2013-06-06 13:48:06 -0500

    Mobile Safari Testing #1

--- js/dancer/src/support.js
+++ js/dancer/src/support.js
@@ -87,6 +87,7 @@
     var
       isApple = !!( navigator.vendor || '' ).match( /Apple/ ),
       version = navigator.userAgent.match( /Version\/([^ ]*)/ );
+      console.log('safari version: ' + version);
     version = version ? parseFloat( version[ 1 ] ) : 0;
     return isApple && version <= 6;
   }
